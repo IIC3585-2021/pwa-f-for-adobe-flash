1ae7cf7ad29bcdb59e54b5539f76e26e0fe9b861
Diego Echeverría <dhecheverria@uc.cl>
2021-05-21 19:26:04 -0400

    Feat: add seed command and remove express staticness

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const admin = require("firebase-admin");
 const firebaseSequelizer = require("firestore-sequelizer");
 const serviceAccount = require("./t4-web-avanzado-firebase-adminsdk-mquh4-4d6254cd6a.json");
+const { User } = require('./src/models');
 
 const httpPort = 8000
 
@@ -12,14 +13,18 @@ admin.initializeApp({
 });
 firebaseSequelizer.initializeApp(admin);
 
+let defaultUser;
+
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'views')))
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'views/index.html'))
+app.get('/', function(req, res, next) {
+  // const chat =
+  console.log(defaultUser)
+  // res.render('index', )
 })
 
 app.listen(httpPort, async function () {
   console.log(`Listening on port ${httpPort}!`)
+  const {data, id} = await User.findOne();
+  defaultUser = {data,id}
 })

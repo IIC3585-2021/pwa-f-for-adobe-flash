78c68a043c0cf35459c9807fde139b546adc95c9
frvalenzuela <frvalenzuela@uc.cl>
2021-05-24 08:15:24 -0400

    something

---
+++ src/public/scripts/change-content.js
@@ -0,0 +1,55 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdF7ItlIDSTLmoiyNL-e-WtlU_SYHizGQ",
+    authDomain: "test-feli.firebaseapp.com",
+    projectId: "test-feli",
+    storageBucket: "test-feli.appspot.com",
+    messagingSenderId: "891051472774",
+    appId: "1:891051472774:web:dbe65b09045ac3be0a5e07",
+    measurementId: "G-LNZ2MJVQJS"
+};
+
+const cafeList = document.querySelector('#chat-list')
+
+function renderDiv(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.id;
+
+    li.appendChild(name);
+
+    cafeList.appendChild(li);
+
+    name.addEventListener('click', (e)=>{
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('message').get().then((snapshot)=>{
+            let ul = document.createElement('ul');
+            li.appendChild(ul)
+            snapshot.docs.forEach(doc1 =>{
+                if (doc1.data().chatId === id) {
+                    let li_c = document.createElement('li');
+                    let content = document.createElement('span');
+                    li_c.setAttribute('data-id', doc1.id);
+                    content.textContent = doc1.data().content;
+                    li_c.appendChild(content);
+                    ul.appendChild(li_c);
+                }
+
+            });
+        })
+    })
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+db.settings({timestampsInSnapshots: true});
+
+// getting data
+db.collection('chat').get().then((snapshot)=>{
+    snapshot.docs.forEach(doc =>{
+        renderDiv(doc)
+    });
+})
--- src/public/sw.js
+++ src/public/sw.js
@@ -1,66 +1,18 @@
-importScripts("https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js");
-
-const cacheName = 'PWA';
-const toCache = [
-  '/',
-  '/manifest.json',
-  '/icon'
-];
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDZjf72IPar7wmZ-ff3_2b0RK32x1-7Mvw",
-  authDomain: "pwa-f-for-adobe-flash.firebaseapp.com",
-  projectId: "pwa-f-for-adobe-flash",
-  storageBucket: "pwa-f-for-adobe-flash.appspot.com",
-  messagingSenderId: "814318270480",
-  appId: "1:814318270480:web:88ec52886ee297e98b3295",
-  measurementId: "G-K7407ZFNEM"
-};
-
-// API push: Cuando firebase manda notificación
-self.addEventListener("push", function (event) {
-    /* Acá se podría hacer una consulta al backend con los detalles de
-    la notificación */
-    console.info("Event: Push");
-    let title = "Bienvenido!";
-    let body = {
-      body: "Te has suscrito a las notificaciones",
-      tag: "pwa",
-      icon: "./images/icon-72x72.png",
-    };
-    event.waitUntil(self.registration.showNotification(title, body));
-  });
-
-
+const cacheName = 'Temporas';
 
 // Cache all the files to make a PWA
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(cacheName)
-    .then(cache => {
-      console.log("Service worker instalado")
+    caches.open(cacheName).then(cache => {
       // Our application only has two files here index.html and manifest.json
       // but you can add more such as style.css as your app grows
-      cache.addAll(toCache);
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './cache.json'
+      ]);
     })
-    .catch(e => {
-      console.log(e)
-    })
-  );
-});
-
-self.addEventListener('activate', function (event) {
-  // clear cache when active
-  // const cacheWhitelist = ['<%= name %>'];
-  event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(keyList.map(key => {
-        // if (cacheWhitelist.indexOf(key) === -1) {
-        return caches.delete(key);
-        // }
-      }))
-    )
   );
 });
 
@@ -68,11 +20,6 @@ self.addEventListener('activate', function (event) {
 // and check if we have cached the file
 // if so it will serve the cached file
 self.addEventListener('fetch', event => {
-  // event.respondWith(
-  //   caches.match(event.request).then(cacheRes => {
-  //     return cacheRes || fetch(event.request);
-  //   })
-  // );
   event.respondWith(
     caches.open(cacheName)
       .then(cache => cache.match(event.request, { ignoreSearch: true }))
